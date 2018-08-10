bb1153438002b6f9b1b22212df684f2ec87ef8da
alex4814 <alex4814.fu@yahoo.com>
2018-08-11 01:27:42 +0800

    Home page with templates

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, world!'
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/yin', methods=['POST'])
+def yin():
+    account = request.form['account']
+    return render_template('yin.html', account=account)
