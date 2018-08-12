c5163207a35ada04b9752720ee4acb0681f472fc
Ruisheng Fu <frsn5903@corp.netease.com>
2018-08-12 22:38:49 +0800

    Realtime progress using EventSource

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Response
 from flask import render_template
 from flask import request
 
@@ -15,9 +16,24 @@ def index():
 @app.route('/yin', methods=['POST'])
 def yin():
     account = request.form['account']
+    targets = [key for key in gdata.category if key in request.form]
     return render_template('yin.html', account=account)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/export/xlsx/<account>')
+def export_xlsx(account):
+    print('export xlsx for account', account)
+    # See also: https://github.com/djdmorrison/flask-progress-example
+    def export():
+        import time
+        x = 0
+        while x <= 100:
+            yield 'data: {}\n\n'.format(x)
+            x += 1
+            time.sleep(0.1)
+    return Response(export(), mimetype='text/event-stream')
